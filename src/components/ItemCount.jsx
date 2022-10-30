85c7dd38e7526bce7849d4892873f702f48cad80
Leandro Nazar <ljnazar@gmail.com>
2022-10-30 14:53:59 -0300

    Feature-Counter-Component

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -3,33 +3,53 @@ import { useState } from 'react';
 
 export default function ItemCount() {
 
+  const stock = 5;
+
   const [count, setCount] = useState(0);
 
   const stockChange = (simbol) => {
-    if(simbol === "+"){
-      setCount(count + 1);
-    }else if(simbol === "-"){
-      setCount(count - 1);
-    } 
+
+    if(count > 0){
+      if(simbol === "+"){
+        setCount(count + 1);
+      }else{
+        setCount(count - 1);
+      }
+    }else if(count == 0){
+      if(simbol === "+"){
+        setCount(count + 1);
+      } 
+    }
+
   };
 
   return (
-    <div>
-        {/* <button className="font-bold text-2xl" id="btn-increase">-</button>
-        <span className="px-3" id="stock">0</span>
-        <button className="font-bold text-2xl" id="btn-decrease">+</button> */}
-        <div className="flex items-baseline my-6">
-            <div className="space-x-3 flex text-sm font-medium" />
-            <label>
-                <input className="sr-only peer" name="size" type="radio" />
-                <div onClick={() => {stockChange("-")}} className="bg-slate-100 font-bold text-xl relative w-10 h-10 flex items-center justify-center text-black peer-checked:bg-black peer-checked:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full peer-checked:before:bg-teal-400">-</div>
-            </label>
-            <span className="px-3 font-bold text-lg" id="stock">{count}</span>
-            <label>
-                <input className="sr-only peer" name="size" type="radio" />
-                <button onClick={() => {stockChange("+")}} className="bg-slate-100 font-bold text-xl relative w-10 h-10 flex items-center justify-center text-black peer-checked:bg-black peer-checked:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full peer-checked:before:bg-teal-400 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">+</button>
-            </label>  
-        </div>
+    <div className="flex-col w-60 m-8 border">
+
+      <p className="flex justify-center font-semibold p-2">Stock disponible {stock}</p>
+
+      <div className="flex justify-center items-baseline p-2">
+        <button onClick={() => {stockChange("-")}} className="bg-slate-100 font-bold text-xl relative w-10 h-10 flex items-center justify-center text-black active:bg-black active:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full active:before:bg-teal-400 hover:bg-teal-400">
+          -
+        </button>
+        <span className="px-3 font-bold">
+          {count}
+        </span>
+        <button onClick={() => {stockChange("+")}} className="bg-slate-100 font-bold text-xl relative w-10 h-10 flex items-center justify-center text-black active:bg-black active:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full active:before:bg-teal-400 hover:bg-teal-400">
+          +
+        </button>
+      </div>
+
+      <div className="flex justify-center space-x-2 text-sm font-medium p-2">
+        <button className="px-6 h-12 font-semibold tracking-wider border border-slate-200 text-slate-900 active:font-bold active:border-2 active:border-black active:before:bg-teal-400 hover:bg-teal-400" type="button">
+          ADD TO CART
+        </button>
+        <button className="flex-none flex items-center justify-center w-12 h-12 text-black" type="button" aria-label="Like">
+          <svg width={20} height={20} fill="currentColor" aria-hidden="true">
+            <path fillRule="evenodd" clipRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+          </svg>
+        </button>
+      </div>
     </div>
 
   )
