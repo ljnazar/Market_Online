d58ddfeee3b0c614b014138cd9dbbe31d840cbee
Leandro Nazar <ljnazar@gmail.com>
2022-10-31 14:27:33 -0300

    Feature-Counter-Component

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -3,16 +3,16 @@ import { useState } from 'react';
 
 export default function ItemCount() {
 
-  const [stock, setStock] = useState(5);
+  const initialStock = 5;
+
+  const [stock, setStock] = useState(initialStock);
 
   const [count, setCount] = useState(0);
 
-  let maxStock = stock;
-
   const stockChange = (simbol) => {
 
-    if(stock > 0 && stock < maxStock){
-      console.log(maxStock);
+    if(stock > 0 && stock < initialStock){
+      console.log("Hay stock");
       if(simbol === "+"){
         setCount(count + 1);
         setStock(stock - 1);
@@ -39,12 +39,27 @@ export default function ItemCount() {
     }*/
 
 
+    }else if(stock === 0){
+      console.log("No hay stock");
+      if(simbol === "-"){
+        setCount(count - 1);
+        setStock(stock + 1);
+      }
+    }else if(stock === initialStock){
+      if(simbol === "+"){
+        setCount(count + 1);
+        setStock(stock - 1);
+      }
+    }
   };
 
   return (
+
     <div className="flex-col w-60 m-8 border">
 
-      <p className="flex justify-center font-semibold p-2">Stock disponible {stock}</p>
+      <p className="flex justify-center font-semibold p-2">
+        Stock disponible {stock}
+      </p>
 
       <div className="flex justify-center items-baseline p-2">
         <button onClick={() => {stockChange("-")}} className="bg-slate-100 font-bold text-xl relative w-10 h-10 flex items-center justify-center text-black active:bg-black active:text-white before:absolute before:z-[-1] before:top-0.5 before:left-0.5 before:w-full before:h-full active:before:bg-teal-400 hover:bg-teal-400">
@@ -68,6 +83,7 @@ export default function ItemCount() {
           </svg>
         </button>
       </div>
+
     </div>
 
   )
